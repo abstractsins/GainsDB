48676cd013a13a11402ce7eab6bbb170dd0565a4
Daniel Berlin <daniel.berlin@barcoding.com>
2025-03-16 19:12:25 -0400

    cors

--- gym-progress-backend/src/server.js
+++ gym-progress-backend/src/server.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
     'https://gainsDB.com',
     'https://www.gainsDB.com',
     'https://gymprogress-development.up.railway.app',
+    'https://gainsdb-prod.up.railway.app',
     'http://localhost:3000',
     'http://10.0.0.116:3000',
     'http://10.0.0.107:3000' // Your phone's IP
