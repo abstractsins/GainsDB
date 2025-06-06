ebb01c7e610cc0a2a74ff6a302cd4d2fd446e37a
Daniel Berlin <daniel.berlin@barcoding.com>
2025-06-05 21:26:02 -0400

    demo popup

--- gym-progress-frontend/app/page.tsx
+++ gym-progress-frontend/app/page.tsx
@@ -43,6 +43,8 @@ export default function Home() {
   // Redirect authenticated users to the dashboard
   useEffect(() => {
 
+    console.log(env);
+
     setIsInRegistration(false);
 
     const checkAuth = async () => {
@@ -79,8 +81,8 @@ export default function Home() {
     checkAuth();
     
   // Optional: log which env is running
-  console.log("ENV:", process.env.VERCEL_ENV);
-  console.log("ENV (frontend):", process.env.NEXT_PUBLIC_VERCEL_ENV);
+  // console.log("ENV:", process.env.VERCEL_ENV);
+  // console.log("ENV (frontend):", process.env.NEXT_PUBLIC_VERCEL_ENV);
 
   }, [status, session, router]);
 
